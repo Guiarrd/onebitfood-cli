28251e5b918511796a09cf58df67b8fc9079d21f
Guilherme Arruda <guilherme_arruda@live.com>
2020-02-25 20:37:53 -0300

    Adicionando Redux Persist para persistir os dados da Store. Adicionado configuraçao para que a aplicaçao trabalhe com o ReduxDevTools

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import Store from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import persistor from './store/persisted_store';
 
 import './index.css';
 
 ReactDOM.render(
   <Provider store={Store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>, 
   document.getElementById('root'));
 serviceWorker.unregister();
