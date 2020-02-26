4ab5f9081e5b49d0f073cdcbc51fed5090fcca22
Guilherme Arruda <guilherme_arruda@live.com>
2020-02-26 01:57:04 -0300

    Criaçao do formulario dos novos pedidos

--- src/screens/create_order/index.js
+++ src/screens/create_order/index.js
@@ -1,13 +1,19 @@
-import React, { Fragment } from 'react';
-import { Column } from 'rbx';
+import React from 'react';
+import { Column, Box } from 'rbx';
+import OrderForm from '../../components/order_form';
+import Order from '../../components/order';
+
+import '../../styles/create_order.scss';
 
 const CreateOrder = (props) => (
   <Column.Group centered>
     <Column size="4" offset="1">
-      Order Form
+      <OrderForm />
     </Column>
-    <Column size="4" offset="1">
-      Product List
+    <Column size="3" offset="1">
+      <Box className="shopping_cart">
+        <Order finish_btn_active={false} />
+      </Box>
     </Column>
   </Column.Group>
 );
